91a5c03363d161d5f7d34b09fdd9328beea9a10b
chenxiaokun <chenxiaokun@didiglobal.com>
2023-03-16 10:41:27 +0800

    ✨ feat: 修改 antd 样式前缀
    修改 antd 样式前缀

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,30 @@ const getWebpackConfig = require('@ant-design/tools/lib/getWebpackConfig');
 
 const { webpack } = getWebpackConfig;
 
+function injectLessVariables(config, variables) {
+  (Array.isArray(config) ? config : [config]).forEach(conf => {
+    conf.module.rules.forEach(rule => {
+      // filter less rule
+      if (rule.test instanceof RegExp && rule.test.test('.less')) {
+        const lessRule = rule.use[rule.use.length - 1];
+        if (lessRule.options.lessOptions) {
+          lessRule.options.lessOptions.modifyVars = {
+            ...lessRule.options.lessOptions.modifyVars,
+            ...variables,
+          };
+        } else {
+          lessRule.options.modifyVars = {
+            ...lessRule.options.modifyVars,
+            ...variables,
+          };
+        }
+      }
+    });
+  });
+
+  return config;
+}
+
 // noParse still leave `require('./locale' + name)` in dist files
 // ignore is better: http://stackoverflow.com/q/25384360
 function ignoreMomentLocale(webpackConfig) {
@@ -29,7 +53,12 @@ function externalMoment(config) {
   };
 }
 
-const webpackConfig = getWebpackConfig(false);
+const legacyEntryVars = {
+  'ant-prefix': 'antd3',
+};
+
+const webpackConfig = injectLessVariables(getWebpackConfig(false), legacyEntryVars);
+
 if (process.env.RUN_ENV === 'PRODUCTION') {
   webpackConfig.forEach(config => {
     ignoreMomentLocale(config);
